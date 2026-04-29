fa3651c5074f7fb3811fdd4881c4f460bc39cadf
Andy Langer <mightydodongo@gmail.com>
2026-04-29 15:11:41 -0500

    Cleanup

--- src/Artifacts/ArtifactPool/ArtifactPool.h
+++ src/Artifacts/ArtifactPool/ArtifactPool.h
@@ -5,54 +5,94 @@
 #ifndef ASTROLOGIAN_ARTIFACTPOOL_H
 #define ASTROLOGIAN_ARTIFACTPOOL_H
 #include "../Artifact.h"
+#include "../../Exceptions/IllegalArgumentException.h"
 #include "../../Util/Random/RandomNumber.h"
 #include "../../ViewFactory/ViewFactory.h"
 
-//TODO. Doc
+/**
+ * Class encapsulating the logic necessary to
+ * create and retrieve a random Artifact from all possible Artifacts
+ */
 class ArtifactPool {
-    //TODO. Document these variables
+    //Class to retrieve a random number.
+    //Needed to select the random Artifact
     RandomNumber& randomNumber;
+
+    //The list of the VIEW_TYPE of every Artifact in the game
+    //Needed so we can randomly select an entry in the vector
     std::vector<VIEW_TYPE> artifacts;
+
+    //The ViewFactory. Needed to construct the View for the created
+    //Artifact
     const ViewFactory& viewFactory;
+
+    /**
+     * Map containing functions that will construct an artifact for the key VIEW_TYPE.
+     * The artifact is returned as a unique_ptr
+     */
     std::unordered_map<VIEW_TYPE, std::function<std::unique_ptr<Artifact>(const ViewFactory&)>> creators;
 
-    //TODO. Doc
-    //TODO. Rename
-    template<typename ArtifactT, typename ViewT> static std::unique_ptr<Artifact>  foo(const VIEW_TYPE viewType, const ViewFactory& viewFactory) {
-        //TODO. Explain what this method is doing
-        //TODO. Confusing name
-        View * v = viewFactory.getView(viewType, false);
+    /**
+     *Template function to construct a unique_ptr for an Artifact for the given VIEW_TYPE and ArtifactT
+     *
+     *@param viewType The VIEW_TYPE to create an Artifact for
+     *@param viewFactory The ViewFactory
+     */
+    template<typename ArtifactT, typename ViewT> static std::unique_ptr<Artifact>  getArtifactForViewType(const VIEW_TYPE viewType, const ViewFactory& viewFactory) {
+        //Get the View for the given VIEW_TYPE. An Artifact must have a View
+        View * view = viewFactory.getView(viewType, false);
 
-        //TODO. Should we nullcheck the pointer
+        //If view is null, that means an incorrect VIEW_TYPE was supplied
+        if (view == nullptr) {
+            std::string errorMessage = "Illegal argument for 'getArtifactForViewType. Cannot create artifact with VIEW_TYPE: ";
+            errorMessage += viewType;
 
-        //TODO. Confusing Name
-        //TODO. Document why we need a dynamic_cast here
-        //TODO. Is it necessary to see if the cast succeeded? Is it nullptr?
-        const auto v2 = dynamic_cast<ViewT*>(v);
+            throw IllegalArgumentException(errorMessage);
+        }
 
-        return std::make_unique<ArtifactT>(*v2);
+        //Need a dynamic cast here; Artifacts will generally only accept references
+        //to View types specific to them. Generic View is generally not allowed
+        const auto viewT = dynamic_cast<ViewT*>(view);
+
+        if (viewT == nullptr) {
+            //Bad cast. Throw an exception
+            std::string errorMessage = "Illegal argument for 'getArtifactForViewType. Cannot cast View for VIEW_TYPE: ";
+            errorMessage += viewType;
+
+            throw IllegalArgumentException(errorMessage);
+        }
+
+        return std::make_unique<ArtifactT>(*viewT);
     }
 
-    //TODO. Doc
+    /**
+     * Get a function that will create an Artifact with type ArtifactT, and return a unique_ptr to it
+     * @tparam ArtifactT The type of the Artifact
+     * @tparam ViewT  The type of the View used to construct the Artifact. Should match ArtifactT
+     * @param type The VIEW_TYPE to create the function for
+     * @return the unique_ptr to the Artifact
+     */
     template<typename ArtifactT, typename ViewT> static std::function<std::unique_ptr<Artifact>(const ViewFactory&)> getEntry(VIEW_TYPE type) {
-        //TODO. Explain what this method is doing and why
-        //TODO. Confusing name vf
-        return [type](const ViewFactory& vf) {
-            return foo<ArtifactT, ViewT>(type, vf);
+        return [type](const ViewFactory& viewFactory) {
+            return getArtifactForViewType<ArtifactT, ViewT>(type, viewFactory);
         };
     }
 
-    //TODO. Doc
-    std::unique_ptr<Artifact> constructArtifactWithType(VIEW_TYPE type) const;
+    [[nodiscard]] std::unique_ptr<Artifact> constructArtifactWithType(VIEW_TYPE type) const;
 
 public:
-    //TODO. Doc
+    /**
+     * Constructor
+     * @param randomNumber The RandomNumber
+     * @param viewFactory  The ViewFactory
+     */
     ArtifactPool(RandomNumber& randomNumber, const ViewFactory& viewFactory);
 
-    //TODO. Doc
+    /**
+     * Get a unique_ptr to a random Artifact
+     * @return the unique_ptr to the created Artifact
+     */
     [[nodiscard]] std::unique_ptr<Artifact> getArtifact() const;
 };
-
-
 
 #endif //ASTROLOGIAN_ARTIFACTPOOL_H
